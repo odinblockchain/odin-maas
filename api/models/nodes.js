b7ecc653fdaac6421bc27bc8bb627190d3dfd5e0
Lance <lancehall123@gmail.com>
2019-01-16 23:10:02 +0000

    refactor of node info

--- api/models/nodes.js
+++ api/models/nodes.js
@@ -3,8 +3,10 @@ const mongoose = require('mongoose');
 const nodeSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     name: String,
-    ipaddress: String,
-    mnprivkey: String,
+    rpcuser: String,
+    rpcpassword: String,
+    nodeip: String,
+    nodeprivkey: String,
     allocated: Boolean
 })
 module.exports = mongoose.model('Node',nodeSchema)
--- api/routes/nodes.js
+++ api/routes/nodes.js
@@ -5,79 +5,64 @@ const RpcController = require('../controllers/rpc')
 const RpcClient = require('node-json-rpc2').Client;
 var dotConf = require("@sim.perelli/dot-conf")
 const Nodes = require('../models/nodes')
-var odinRpc = "";
 var path = './odin_n1.conf'
+const mongoose = require('mongoose')
 
 noderouter.get("/", NodeController.nodes_get_all)
 
-async function rpcclient()
-{
-    const rpccreds = getrpcconfigs();
-    odinRpc = new RpcClient(rpccreds);
-}
-
-async function getrpcconfigs(res) {
+async function getrpcconfigs(req,res) {
     const s = await dotConf(path)
     var rpcuser = s('rpcuser')
     var rpcpassword = s('rpcpassword')
     RpcConfig = {
-        rpcuser: rpcuser,
-        rpcpassword:rpcpassword,
-        rpcport: 1988
+        user: rpcuser,
+        password:rpcpassword,
+        port: 1988
     }
-        res.status(200).json(RpcConfig)
+        return RpcConfig
 }
 
 
 noderouter.get("/odin_n1/config", (req, res) => {
-    Nodes.find()
-        .exec()
-        .then(docs => {
-            console.log(docs);
-            if (docs.length >= 0) {
-                res.status(200).json(docs);
-            } else {
-                res.status(404).json({
-                    message: 'No entries found'
-                });
-            }
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json({
-                error: err
-            });
-        });
-  //  const nodeConfig = {
-   //     name: "odin_n1",
-    //    nodeip: bind,
-   //     nodeprivkey: masternodeprivkey
-  //  }
-
-  // odinRpc = new RpcClient(RpcConfig);
-    res.status(200).json(nodeConfig)
-})
-noderouter.get("/odin_n1/status", function (req, res) {
-    odinRpc.call({
-      method: 'masternode',
-      params: ["genkey"]
-    }, (err, result) => {
-      if (err) {
-        res.status(500).json({
-          error: err.message
-        })
-      }
-      res.send({ body: result.result })
+    Nodes.find({name: "odin_n1"},function(err,config){
+        if (err){
+        res.send(err);
+    }
+    res.status(200).json(config);
     })
     
-  })
+})
+noderouter.get("/odin_n1/status", function (req, res) {
+    const rpccreds = getrpcconfigs()
+    rpccreds
+    .then(
+        docs => {
+            odinRpc = new RpcClient(docs);
+            odinRpc.call({
+              method: 'masternode',
+              params: ["genkey"]
+            }, (err, result) => {
+              if (err) {
+                res.status(500).json({
+                  error: err.message
+                })
+              }
+              res.status(200).json(result.result)})
+             //return result.result
+            })
+            
+          })
+   // .catch()    
+    
+
+   
 
 noderouter.post("/", async function(req, res){
-
-
     const node = new Nodes({
         _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
+        rpcuser: req.body.user,
+        rpcpassword: req.body.password,
         nodeip: req.body.nodeip,
         nodeprivkey: req.body.nodeprivkey,
         allocated: false
